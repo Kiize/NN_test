e1ff1c3db1aa44262f156b5ed0d29f962f853be1
Kiize <vitogallo99@gmail.com>
2023-10-24 20:01:18 +0200

    add Layer_Dense

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import numpy as np
 import time
 
 start = time.time()
+np.random.seed(0)
 
 X = [
     [1, 2, 3, 2.5],
@@ -9,6 +10,18 @@ X = [
     [-1.5, 2.7, 3.3, -0.8]
 ]
 
+class Layer_Dense:
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
 
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
+
+layer1 = Layer_Dense(4, 5)
+layer2 = Layer_Dense(5, 2)
+
+layer1.forward(X)
+print(layer1.output)
 
 print("elapsed time: %.1f seconds" % (time.time() - start))
