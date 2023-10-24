4b849f8b1b01978ab79776ac8170a6ee315a69ab
Kiize <vitogallo99@gmail.com>
2023-10-24 22:18:41 +0200

    add Activation_ReLU and create_data

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt 
 import time
 
 start = time.time()
@@ -18,10 +19,32 @@ class Layer_Dense:
     def forward(self, inputs):
         self.output = np.dot(inputs, self.weights) + self.biases
 
-layer1 = Layer_Dense(4, 5)
-layer2 = Layer_Dense(5, 2)
+class Activation_ReLU:
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
 
-layer1.forward(X)
-print(layer1.output)
+def create_data(points, classes):
+    x = np.zeros((points*classes, 2))
+    y = np.zeros(points*classes, dtype='uint8')
 
-print(f"elapsed time: {(time.time() - start):.1f} seconds")
+    for class_number in range(classes):
+        ix = range(points*class_number, points*(class_number + 1))
+        r = np.linspace(.0, 1, points)
+        t = np.linspace(class_number*4, (class_number + 1)*4, points) + np.random.randn(points) * .2
+        x[ix] = np.c_[r*np.sin(2.5 * t), r*np.cos(2.5*t)]
+        y[ix] = class_number
+
+    return x, y
+
+x, y = create_data(100, 3)
+
+layer1 = Layer_Dense(2, 5)
+activation1 = Activation_ReLU()
+
+layer1.forward(x)
+activation1.forward(layer1.output)
+
+#print(layer1.output)
+print(activation1.output)
+
+print(f"elapsed time: {(time.time() - start):.1f} seconds\n")
