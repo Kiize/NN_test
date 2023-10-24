97e5b46870e8916b9cd0dbe80ebc71a5bea8e747
Kiize <vitogallo99@gmail.com>
2023-10-24 19:45:22 +0200

    add test.py

---
+++ test.py
@@ -0,0 +1,14 @@
+import numpy as np
+import time
+
+start = time.time()
+
+X = [
+    [1, 2, 3, 2.5],
+    [2, 5, -1, 2],
+    [-1.5, 2.7, 3.3, -0.8]
+]
+
+
+
+print("elapsed time: %.1f seconds" % (time.time() - start))
